e6817fa8a253eeffbe285b9cf50c265884e82e60
Victor Nery <victor.teixeira@zflow.com.br>
2020-04-01 02:08:26 -0300

    Criando nova rota no meninao - reports

--- src/components/Routing/index.js
+++ src/components/Routing/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Fragment} from 'react'
 import { Route, NavLink } from 'react-router-dom'
 import styled from 'styled-components'
 
@@ -6,18 +6,20 @@ import Index from '@containers/index'
 import CreateCampaign from '@containers/createCampaign'
 import Me from '@containers/me'
 import Register from '@containers/register'
+import Reports from '@containers/reports'
 
 const NavItem = styled(NavLink)`
   background: red;
 `
 
 const Routing = () => (
-  <div>
+  <Fragment>
     <Route exact path="/" component={Index} />
     <Route exact path="/create-campaign" component={CreateCampaign} />
     <Route exact path="/me" component={Me} />
     <Route exact path="/register" component={Register} />
-  </div>
+    <Route exact path="/reports" component={Reports} />
+  </Fragment>
 )
 
 export default Routing
---
+++ src/containers/reports/index.js
@@ -0,0 +1,14 @@
+import React, { Component, Fragment } from 'react'
+
+class Reports extends Component {
+
+  render() {
+    return (
+      <Fragment>
+      Sou um report
+      </Fragment>
+    )
+  }
+}
+
+export default Reports
